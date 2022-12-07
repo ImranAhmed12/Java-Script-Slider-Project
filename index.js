20c7ee2874b24bc35b2d7aebcced295046585228
imranAhmed12 <imranahmed795@gmail.com>
2022-12-07 18:41:06 +0530

    first commit

--- index.js
+++ index.js
@@ -1,65 +1,19 @@
-const left = document.querySelector(".left");
-const right = document.querySelector(".right");
-const slider = document.querySelector(".slider");
-const images = document.querySelectorAll(".image");
-const bottom = document.querySelector(".bottom");
+const end_date= "28 December 2025 5:23 PM"; 
+const inputs = document.querySelectorAll("input")
+document.getElementById("end-date").innerHTML=end_date;
 
-let slideNumber = 1;
-const length = images.length;
-
-for (let i = 0; i < length; i++) {
-  const div = document.createElement("div");
-  div.className = "button";
-  bottom.appendChild(div);
-}
-
-const buttons = document.querySelectorAll(".button");
-buttons[0].style.backgroundColor = "white";
-
-const resetBg = () => {
-  buttons.forEach((button) => {
-    button.style.backgroundColor = "transparent";
-  });
-};
-
-buttons.forEach((button, i) => {
-  button.addEventListener("click", () => {
-    resetBg();
-    slider.style.transform = `translateX(-${i * 800}px)`;
-    slideNumber = i + 1;
-    button.style.backgroundColor = "white";
-  });
-});
-
-const nextSlide = () => {
-  slider.style.transform = `translateX(-${slideNumber * 800}px)`;
-  slideNumber++;
-};
-const prevSlide = () => {
-  slider.style.transform = `translateX(-${(slideNumber - 2) * 800}px)`;
-  slideNumber--;
-};
-const getFirstSlide = () => {
-  slider.style.transform = `translateX(0px)`;
-  slideNumber = 1;
-};
-const getLastSlide = () => {
-  slider.style.transform = `translateX(-${(length - 1) * 800}px)`;
-  slideNumber = length;
-};
-
-const changeColor = ()=>{
-  resetBg()
-  buttons[slideNumber-1].style.backgroundColor = "white";
-}
-
-right.addEventListener("click", () => {
-  slideNumber < length ? nextSlide() : getFirstSlide();
-  changeColor()
-});
-
-left.addEventListener("click", () => {
-  slideNumber > 1 ? prevSlide() : getLastSlide();
-  changeColor()
-});
-
+const clock=()=>{
+  const end = new Date(end_date)
+  const now= new Date()
+  const diff = (end-now)/1000;
+  console.log(diff);
+  inputs[0].value=Math.floor(diff/3600/24);
+  inputs[1].value=Math.floor(diff/3600)%24;
+  inputs[2].value=Math.floor(diff/60)%60;
+  inputs[3].value=Math.floor(diff)%60;
+  
+} 
+clock()
+setInterval(()=>{
+  clock()
+},1000)
